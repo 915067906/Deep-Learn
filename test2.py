ec3a95b2b8c878bb9d6e013c09a17560dd628cbc
一念如梦 <915067906@qq.com>
2021-04-30 20:01:55 +0800

    测试添加

---
+++ test2.py
@@ -0,0 +1,13 @@
+'''
+Author: your name
+Date: 2021-04-30 20:00:38
+LastEditTime: 2021-04-30 20:01:12
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \DL\test2.py
+'''
+
+
+import tensorflow 
+
+print(tensorflow)
