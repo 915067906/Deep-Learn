ab92efc791140d485f78146ca7c173af67f303cd
一念入梦 <915067906@qq.com>
2021-04-30 23:51:22 +0800

    mac添加

---
+++ mac.py
@@ -0,0 +1,10 @@
+'''
+Author: your name
+Date: 2021-04-30 23:33:54
+LastEditTime: 2021-04-30 23:34:11
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /ML/mac.py
+'''
+import sys
+print(111)
