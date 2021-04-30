2069ccac1c4ea8a8af9966853efd53cf00164b90
一念如梦 <915067906@qq.com>
2021-04-30 19:46:50 +0800

    test测试

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+Author: your name
+Date: 2021-04-30 19:37:23
+LastEditTime: 2021-04-30 19:38:07
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \DL\test.py
+'''
+import cv2
+
+print(1)
+print(2)
